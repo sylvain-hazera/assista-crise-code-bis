10fd703999492419c0aa664cc4a0b1383c70828f
Stefy237 <ngueganglaura@gmail.com>
2026-01-26 11:14:15 +0100

    [UPDATE] dashboard

--- frontend/src/app/layouts/admin-layout/admin-layout.component.ts
+++ frontend/src/app/layouts/admin-layout/admin-layout.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive, Router } from '@angular/router';
 import { AuthService } from '../../auth/services/auth.service';
 import { User } from '../../shared/models/user.model';
-// import { AuthMockService, User } from '../../shared/services/auth-mock.service';
 
 interface NavItem {
   icon: string;
