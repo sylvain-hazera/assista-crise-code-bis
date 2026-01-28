94e00402249a8f83096ed47768ec9b7ac2a5d429
Stefy237 <ngueganglaura@gmail.com>
2026-01-28 15:40:20 +0100

    [UPDATE] Affichage des vraies données sur le dashboard admin

--- frontend/src/app/admin/dashboard/dashboard.component.ts
+++ frontend/src/app/admin/dashboard/dashboard.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
 import { forkJoin, Subject, takeUntil } from 'rxjs';
 import { CrisisService } from '../../services/crisis.service';
