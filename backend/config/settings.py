537204cb134603d4fc311ea6b614aecaa9170f78
Maxime Bossant <maxime.bossant@gmail.com>
2026-02-06 14:03:10 +0100

    roots of mail implementation before merging on test

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -191,3 +191,12 @@ SIMPLE_JWT = {
 }
 
 AUTH_USER_MODEL = 'core.Utilisateur'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'mailhog'  
+EMAIL_PORT = 1025
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = False
+
+DEFAULT_FROM_EMAIL = 'ne-pas-repondre@assista-crise.fr'
