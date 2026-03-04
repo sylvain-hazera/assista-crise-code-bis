98d4c2944959044ec32d72983f7a93137758a4ef
Maxime Bossant <maxime.bossant@gmail.com>
2026-03-04 09:58:23 +0100

    push on prod before moving on the new vm

--- backend/core/views.py
+++ backend/core/views.py
@@ -277,7 +277,7 @@ class UserViewSet(viewsets.ModelViewSet):
 class CrisisViewSet(viewsets.ModelViewSet):
     queryset = Crisis.objects.all()
     serializer_class = CrisisSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [AllowAny] 
     filterset_class = AuthorEmailFilter
 
 class RequestViewSet(viewsets.ModelViewSet):
