6395499ef12fd56fca6e0d354b45096fe83c709e
Maxime Bossant <maxime.bossant@gmail.com>
2026-03-10 14:56:36 +0100

    popup to notify auth and rescue that their account are not validated by an admin

--- frontend/src/app/core/guards/enable.guard.ts
+++ frontend/src/app/core/guards/enable.guard.ts
@@ -11,5 +11,10 @@ export const enableGuard: CanActivateFn = (route, state) => {
     return true; // Accès autorisé
   }
 
+  // Afficher un message d'information à l'utilisateur
+  alert(
+    'Votre compte est en attente de validation par un administrateur.\n\n'
+  );
+
   return router.parseUrl('/accueil'); 
 };
